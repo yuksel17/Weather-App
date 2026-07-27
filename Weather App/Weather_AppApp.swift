f96000cc6e991cbc06ec79c7b1c7e9c63314beb1
yuksel <210504020@st.atlas.edu.tr>
2026-07-27 18:03:06 +0300

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Yüksel Işıklar on 27.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
